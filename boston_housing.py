1f4c0752af559b95a977f063ca97560b423a5f89
Anirudh Jain <cyna@Anirudhs-MacBook-Pro.local>
2019-07-07 14:01:01 +0530

    clear up useless commands and fix type with MEDV

--- boston_housing.py
+++ boston_housing.py
@@ -2,11 +2,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
-import os
-os.chdir('C:\\Users\\Sarthak\\Desktop\\ML Project\\boston-housing\\')
+
 boston=pd.read_csv('train.csv')
 boston.head()
 print(boston)
 sns.set(rc={'figure.figsize':(11.7,8.27)})
-sns.distplot(boston['MEDV'], bins=30)
+sns.distplot(boston['medv'], bins=30)
 plt.show()
